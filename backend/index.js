b6cee3303fa5dbbafe0a829a10528c62f22da713
Pieter Vardi <pietervardi@gmail.com>
2023-04-04 17:52:19 +0700

    session & beberapa tambahan lainnya

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const FileUpload = require('express-fileupload');
+const cors = require('cors');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize');
 const dotenv = require('dotenv');
 
 const ContactRoute = require('./routes/ContactRoute');
@@ -8,17 +12,34 @@ const db = require('./config/Database');
 dotenv.config();
 const app = express();
 
+const sessionStore = SequelizeStore(session.Store);
+const store = new sessionStore({ db });
+
 db.sync();
 
+app.use(session({
+  secret: process.env.SESS_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: store,
+  cookie: {
+    secure: 'auto'
+  }
+}))
+
 app.use(cors({
-  credetials: true,
+  credentials: true,
   origin: 'http://localhost:3000'
 }));
 
 app.use(express.json());
+app.use(FileUpload());
+app.use(express.static('public'));
 
 app.use(ContactRoute);
 app.use(AuthRoute);
+
+store.sync();
 
 app.listen(process.env.APP_PORT, () => {
   console.log(`Listening to port ${process.env.APP_PORT}`);
