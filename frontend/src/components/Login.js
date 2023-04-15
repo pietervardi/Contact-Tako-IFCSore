45bf135ba0458665cb0a45f46946006df611b99f
JecksenSTK <jecksensantoko17@gmail.com>
2023-04-15 20:10:20 +0700

    menambahkan halaman login dan register beserta cssnya

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { LoginUser, reset } from '../features/authSlice';
+import '../style/Login.css'
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user, isError, isSuccess, isLoading, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (user || isSuccess) {
+      navigate('/');
+    }
+    dispatch(reset());
+  }, [user, isSuccess, dispatch, navigate]);
+
+  const Auth = (e) => {
+    e.preventDefault();
+    dispatch(LoginUser({ email, password }));
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className='container1'>
+      <form onSubmit={Auth} className='form1'>
+        {isError && <p className='formMessage'>{message}</p>}
+        <h1 className='formHeader'>Sign In</h1>
+        <div className='email1'>
+          <label className='emailLabel'>Email</label>
+          <div>
+            <input
+              type='text'
+              className='input1'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder='Email'
+            />
+          </div>
+        </div>
+        <div className='password1'>
+          <label className='passwordLabel'>Password</label>
+          <div>
+            <input
+              type='password'
+              className='input1'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder='Enter Password'
+            />
+          </div>
+        </div>
+        <div>
+          <button 
+            type='submit'
+            className='submit2'>
+            {isLoading ? 'Loading...' : 'Login'}
+            </button>
+        </div>
+        <div className='register1'>
+          <Link to='/register' className=''>
+            Register
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,9 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+import '../style/Register.css'
 
 const Register = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confPassword, setConfPassword] = useState('');
+  const [msg, setMsg] = useState('');
+  const navigate = useNavigate();
+
+  const saveUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:5000/register', {
+        name: name,
+        email: email,
+        password: password,
+        confPassword: confPassword,
+      });
+      navigate('/login');
+    } catch (error) {
+      if (error.response) {
+        setMsg(error.response.data.msg);
+      }
+    }
+  };
   return (
-    <div>Register</div>
-  )
-}
+    <div className='container1'>
+      <div className='form-container'>
+      <div className='form-header'>Sign Up</div>
+        <form onSubmit={saveUser}>
+          <p className='message'>{msg}</p>
+          <div className='form-group'>
+            <label className='form-label'>Name</label>
+            <div className='form-input'>
+              <input
+                type='text'
+                className='input-field'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder='Name'
+              />
+            </div>
+          </div>
+          <div className='form-group'>
+            <label className='form-label'>Email</label>
+            <div className='form-input'>
+              <input
+                type='text'
+                className='input-field'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder='Email'
+              />
+            </div>
+          </div>
+          <div className='form-group'>
+            <label className='form-label'>Password</label>
+            <div className='form-input'>
+              <input
+                type='password'
+                className='input-field'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder='Enter Password'
+              />
+            </div>
+          </div>
+          <div className='form-group'>
+            <label className='form-label'>Confirm Password</label>
+            <div className='form-input'>
+              <input
+                type='password'
+                className='input-field'
+                value={confPassword}
+                onChange={(e) => setConfPassword(e.target.value)}
+                placeholder='Enter Password'
+              />
+            </div>
+          </div>
+          <div className='form-group'>
+            <div className='form-action'>
+              <button type='submit' className='save-button'>
+                Save
+              </button>
+              <div className='back-link'>
+                <Link to='/login' >
+                  Back
+                </Link>
+              </div>
+            
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Register;
