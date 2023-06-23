c5d691b2688e0acef9cf5397e1006898825b1385
Michael Lay <michaellay1912@yahoo.com>
2023-06-23 21:28:15 +0700

    LOGIN SCREEN DONE

--- mobile/lib/screen/login_screen.dart
+++ mobile/lib/screen/login_screen.dart
@@ -14,33 +14,113 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          child: Column(
-            children: [
-              const Text('Form Login'),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const ContactPage(),
+        child: Center(
+          child: Container(
+            width: 350,
+            height: 425,
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(color: Colors.grey, blurRadius: 20, spreadRadius: 3)
+                ]),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 25),
+                    child: Center(
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
                     ),
-                  );
-                }, 
-                child: const Text('Login')
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const RegisterScreen(),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 30),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15),
+                          child: Text(
+                            "Email",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 19),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15),
+                          child: TextField(
+                              decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Email',
+                          )),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15),
+                          child: Text(
+                            "Password",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 19),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 20),
+                          child: TextField(
+                              decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Enter Password',
+                          )),
+                        ),
+                        Center(
+                          child: ElevatedButton(
+                              style: ButtonStyle(
+                                  padding:
+                                      MaterialStateProperty.all<EdgeInsets>(
+                                          EdgeInsets.symmetric(
+                                              vertical: 15, horizontal: 112)),
+                                  backgroundColor:
+                                      MaterialStatePropertyAll<Color>(
+                                          Colors.greenAccent)),
+                              onPressed: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => const ContactPage(),
+                                  ),
+                                );
+                              },
+                              child: const Text(
+                                'Login',
+                                style: TextStyle(fontSize: 25),
+                              )),
+                        ),
+                        Center(
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 15),
+                            child: TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          const RegisterScreen(),
+                                    ),
+                                  );
+                                },
+                                child: const Text(
+                                  'Register',
+                                  style: TextStyle(
+                                      color: Colors.greenAccent, fontSize: 30),
+                                )),
+                          ),
+                        )
+                      ],
                     ),
-                  );
-                }, 
-                child: const Text(
-                  'Register',
-                )
-              )
-            ]
+                  ),
+                ]),
           ),
         ),
       ),
