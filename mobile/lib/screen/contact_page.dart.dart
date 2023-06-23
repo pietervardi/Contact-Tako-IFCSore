76dda176e9ce6050d5abb81fb073d6796c946582
Pieter Vardi <pietervardi@gmail.com>
2023-06-23 21:54:34 +0700

    Merancang Contact Page (Flutter)

--- mobile/lib/screen/contact_page.dart.dart
+++ mobile/lib/screen/contact_page.dart.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/components/appbar.dart';
+import 'package:mobile/components/contactcard.dart';
 import 'package:mobile/components/title.dart';
 import 'package:mobile/components/welcome.dart';
 import 'package:mobile/screen/formaddcontact_screen.dart';
-import 'package:mobile/screen/formeditcontact_screen.dart';
 
 class ContactPage extends StatefulWidget {
   const ContactPage({Key? key}) : super(key: key);
@@ -13,6 +13,90 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
+  final TextEditingController _searchController = TextEditingController();
+
+  List<Map<String, dynamic>> contacts = [
+    {
+      'name': 'Pieter Vardi',
+      'phone': '08123456789',
+      'address': 'Jalan Asia no 15A',
+      'image': 'assets/comb_over.jpg',
+    },
+    {
+      'name': 'Jecksen Santoko',
+      'phone': '08123456798',
+      'address': 'Jalan Amerika no 18',
+      'image': 'assets/comma_hair.jpg',
+    },
+    {
+      'name': 'Victor',
+      'phone': '08987654321',
+      'address': 'Jalan Krakatau no 19C',
+      'image': 'assets/barber1.jpg',
+    },
+    {
+      'name': 'Louis',
+      'phone': '08231456121',
+      'address': 'Jalan Bintang no 2Y',
+      'image': 'assets/fluffy.jpg',
+    },
+    {
+      'name': 'Nicholas Chandra',
+      'phone': '08192836183',
+      'address': 'Jalan Afrika no 20A',
+      'image': 'assets/french_crop.jpg',
+    },
+    {
+      'name': 'Hansen Tanio',
+      'phone': '081927351868',
+      'address': 'Jalan Sunggal no 26C',
+      'image': 'assets/two_blocks.jpeg',
+    },
+    {
+      'name': 'Vincent Chaislie',
+      'phone': '08231456333',
+      'address': 'Jalan Cemara no 10',
+      'image': 'assets/buzzcut.jpg',
+    },
+    {
+      'name': 'Antony',
+      'phone': '082314545410',
+      'address': 'Jalan Merauke no 1F',
+      'image': 'assets/barber2.jpeg',
+    },
+    {
+      'name': 'Mario',
+      'phone': '081178374814',
+      'address': 'Jalan Kambing no 22G',
+      'image': 'assets/undercuts.jpeg',
+    },
+    {
+      'name': 'Owen Mastan',
+      'phone': '081383838911',
+      'address': 'Jalan Harimau no 2D',
+      'image': 'assets/mullet.jpg',
+    },
+  ];
+
+  List<Map<String, dynamic>> filteredContacts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    filteredContacts = contacts;
+  }
+
+  void filterContacts(String searchQuery) {
+    setState(() {
+      filteredContacts = contacts
+        .where((contact) =>
+          contact['name']!.toLowerCase().contains(searchQuery) ||
+          contact['phone']!.toLowerCase().contains(searchQuery) ||
+          contact['address']!.toLowerCase().contains(searchQuery))
+        .toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,10 +105,20 @@ class _ContactPageState extends State<ContactPage> {
         child: Padding(
           padding: const EdgeInsets.all(15.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const Welcome(),
               const SizedBox(height: 20,),
               const TitleApp(),
+              const SizedBox(height: 25,),
+              const Text(
+                'List of Contacts',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500
+                ),
+              ),
+              const SizedBox(height: 10,),
               ElevatedButton(
                 onPressed: () {
                   Navigator.of(context).push(
@@ -35,16 +129,34 @@ class _ContactPageState extends State<ContactPage> {
                 }, 
                 child: const Text('Add New')
               ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const FormEditContact(),
-                    ),
-                  );
-                }, 
-                child: const Text('Edit')
+              const SizedBox(height: 10,),
+              Container(
+                margin: const EdgeInsets.only(bottom: 20),
+                child: TextField(
+                  controller: _searchController,
+                  decoration: const InputDecoration(
+                    labelText: 'Search Contacts...',
+                    border: OutlineInputBorder(),
+                  ),
+                  onChanged: filterContacts,
+                ),
               ),
+              Column(
+                children: [
+                  for (var contact in filteredContacts)
+                    Column(
+                      children: [
+                        ContactCard(
+                          name: contact['name']!,
+                          phone: contact['phone']!,
+                          address: contact['address']!,
+                          image: contact['image']!,
+                        ),
+                        const SizedBox(height: 10),
+                      ],
+                    ),
+                ],
+              )
             ],
           ),
         ),
--- mobile/lib/screen/formeditcontact_screen.dart
+++ mobile/lib/screen/formeditcontact_screen.dart
@@ -4,26 +4,65 @@ import 'package:mobile/components/title.dart';
 import 'package:mobile/components/welcome.dart';
 
 class FormEditContact extends StatefulWidget {
-  const FormEditContact({Key? key}) : super(key: key);
+  final Map<String, dynamic> contact;
+  const FormEditContact({Key? key, required this.contact}) : super(key: key);
 
   @override
   State<FormEditContact> createState() => _FormEditContactState();
 }
 
 class _FormEditContactState extends State<FormEditContact> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    nameController.text = widget.contact['name'];
+    phoneController.text = widget.contact['phone'];
+    addressController.text = widget.contact['address'];
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    phoneController.dispose();
+    addressController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(context),
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(15.0),
+          padding: const EdgeInsets.all(15.0),
           child: Column(
             children: [
-              Welcome(),
-              SizedBox(height: 20,),
-              TitleApp(),
-              Text('Edit Contact')
+              const Welcome(),
+              const SizedBox(height: 20),
+              const TitleApp(),
+              const Text('Edit Contact'),
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                ),
+              ),
+              TextField(
+                controller: phoneController,
+                decoration: const InputDecoration(
+                  labelText: 'Phone',
+                ),
+              ),
+              TextField(
+                controller: addressController,
+                decoration: const InputDecoration(
+                  labelText: 'Address',
+                ),
+              ),
             ],
           ),
         ),
